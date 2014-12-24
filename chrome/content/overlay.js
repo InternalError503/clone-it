ce4e298b7e3d83261e64e5fdb374a3f1b0b74ef9
Toady <support@8pecxstudios.com>
2014-12-25 01:57:53 +1000

    Version 1.0.3

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -2,11 +2,14 @@
 // Use of this code is strictly by permissions only even tho its very simple code. \\
 
 //Import Browser Console
-let console = (Cu.import("resource://gre/modules/devtools/Console.jsm", {})).console;
+var iDebuggerModi = false;
+if (iDebuggerModi){
+	let console = (Cu.import("resource://gre/modules/devtools/Console.jsm", {})).console;
+}
 
 var cloneThisTabToNewTab = {
 	//Clones current tab into new tab
-	CloneCurrentTab : function() {
+	CloneCurrentTab : function(e) {
 	//Added in some general error handling by encapsulating it in try catch statements
 	try{
 	//Check if url bar has a url
@@ -15,25 +18,34 @@ var cloneThisTabToNewTab = {
 	var UrlToClone = gURLBar.value;{
 	//Use the URL data to open in new tab
 	gBrowser.selectedTab = openUILinkIn(UrlToClone, 'tab');	
+	
+	if (iDebuggerModi){
 		//Log to browser console upon success
 		console.log("Clone Tab Success");
+		}
 				}
 			}else{
+			
+			if (iDebuggerModi){
 				//Log to browser console if no url data to clone
 				console.log("No Tab To Clone");
+			}
 				}
 		}catch (e){
 			//Catch any nasty errors and output to dialogue and console
 			alert("A Error Was Encountered While Trying To Clone The Tab!" + e);
+			
+			if (iDebuggerModi){
 				//Log to browser console upon failure
 				console.log("Clone Tab Failed" + e);
+			}
 		}
 	}
 }
 
 var cloneThisTabToNewWindow = {	
 	//Clones current tab in new window	
-	CloneCurrentTabNewWindow : function() {
+	CloneCurrentTabNewWindow : function(e) {
 	//Added in some general error handling by encapsulating it in try catch statements		
 	try{
 	//Check if url bar has a url
@@ -42,18 +54,27 @@ var cloneThisTabToNewWindow = {
 	var UrlToClone = gURLBar.value;{
 	//Use the URL data to open in new Window
 	gBrowser.selectedTab = openUILinkIn(UrlToClone, 'window');
+	
+	if (iDebuggerModi){
 		//Log to browser console upon success	
 		console.log("Clone Window Success");
+		}
 				}
 			}else{
+			
+			if (iDebuggerModi){
 				//Log to browser console if no url data to clone	
 				console.log("No Window To Clone");
+			}
 				}
 		}catch (e){
 			//Catch any nasty errors and output to dialogue and console
 			alert("A Error Was Encountered While Trying To Clone The Window!" + e);
+			
+			if (iDebuggerModi){
 				//Log to browser console upon failure
 				console.log("Clone Window Failed" + e);
+			}
 		}		
 	}
 }	
