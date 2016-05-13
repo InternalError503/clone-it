413af9626cd5ed262c40e9061fc25361815fe6fb
toady <support@8pecxstudios.com>
2016-05-13 23:03:00 +1000

    Compress and reduce code while simplifying construct.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,33 +1,11 @@
 (function(global){
 cloneIT = {
-
-CloneCurrentTab : function(e) {
-try{
-
-	if (gURLBar.value){
-	var UrlToClone = gURLBar.value;{
-		gBrowser.selectedTab = openUILinkIn(UrlToClone, 'tab', { relatedToCurrent: true });		
+	CloneCurrent : function(type) {
+	try{
+			if (gURLBar.value){
+				gBrowser.selectedTab = openUILinkIn(gURLBar.value, type, { relatedToCurrent: true });	
+			}
+		}catch(e){}
 	}
-		}
-			}catch (e){
-			//Catch any nasty errors and output to dialogue
-			alert("Were sorry, Something has gone wrong trying to clone the tab! " + e);			
-		}
-	},
-
-CloneCurrentTabNewWindow : function(e) {	
-try{
-
-	if (gURLBar.value){	
-	var UrlToClone = gURLBar.value;{
-		gBrowser.selectedTab = openUILinkIn(UrlToClone, 'window');
-	}
-		}
-			}catch (e){
-			//Catch any nasty errors and output to dialogue
-			alert("Were sorry, Something has gone wrong trying to clone the current tab to a new window! " + e);
-		}		
-	}
-	
-}	
+}
 }(this));	
